75600dd9d2da24fcc5c79bf66388ff1fc6eb9c55
nps2000 <NpsPlayer2000@gmail.com>
2019-01-20 17:34:42 +0300

    Add commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
     //TODO Add a new method 1
     //TODO Add a new method 2
+    //commit
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!");
