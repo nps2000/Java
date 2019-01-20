d661339d917f517897e8f0b0c3d56d9b698482d3
nps2000 <NpsPlayer2000@gmail.com>
2019-01-20 17:08:15 +0300

    Initial commit (Hello World!)

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    //TODO Add a new method 1
+    //TODO Add a new method 2
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+
+    }
+}
